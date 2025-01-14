9221a2ec937b11ef6cc62213b3f2552df0da28b3
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-14 22:21:56 +0700

    feat: (3042-count-prefix-and-suffix-pairs) optimize solution - update check isPrefixAndSuffix

--- easy/3042-count-prefix-and-suffix-pairs.kt
+++ easy/3042-count-prefix-and-suffix-pairs.kt
@@ -1,8 +1,8 @@
 class Solution {
     fun countPrefixSuffixPairs(words: Array<String>): Int {
         var count = 0
-        for (i in 0 until words.size - 1) {
-            for (j in i+1 until words.size) {
+        for (i in words.indices) {
+            for (j in i+1 ..<words.size) {
                 if (isPrefixAndSuffix(words[i], words[j])) {
                     count++
                 }
@@ -13,6 +13,11 @@ class Solution {
 
     fun isPrefixAndSuffix(str1: String, str2: String): Boolean {
         if (str1.length > str2.length) return false
-        return str1 == str2.substring(0, str1.length) && str1 == str2.substring(str2.length - str1.length, str2.length)
+        
+        for (i in str1.indices) {
+            if (str1[i] != str2[i] || str1[str1.length - i - 1] != str2[str2.length - i - 1]) return false
+        }
+
+        return true
     }
 }
