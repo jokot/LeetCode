b3e43005041930f0b985c69b1640700fdba2a489
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-14 14:40:15 +0700

    feat: (1408-string-matching-in-an-array) optimize solution

--- easy/1408-string-matching-in-an-array.kt
+++ easy/1408-string-matching-in-an-array.kt
@@ -1,17 +1,43 @@
 class Solution {
     fun stringMatching(words: Array<String>): List<String> {
-        val pairs = mutableListOf<String>()
+        val sb = StringBuilder()
+        words.forEach {
+            sb.append(it)
+            sb.append("_")
+        }
+        val allWords = sb.toString()
 
-        for (i in 0 until words.lastIndex) {
-            for (j in i + 1 until words.size) {
-                if (words[i].contains(words[j])) {
-                    pairs.add(words[j])
-                } else if (words[j].contains(words[i])) {
-                    pairs.add(words[i])
-                }
+        val subs = mutableListOf<String>()
+        for (word in words) {
+            if (countWordIn(allWords, word) > 1) {
+                subs.add(word)
             }
         }
 
-        return pairs.distinct()
+        return subs
+    }
+
+    fun countWordIn(words: String, search: String): Int {
+        var start = 0
+        var end = words.length - search.length
+
+        var count = 0
+
+        for (i in 0 .. end) {
+            if (
+                words[i] == search[0] 
+                && words[i+search.lastIndex] == search[search.lastIndex]
+            ) {
+                for (j in 1 until search.lastIndex) {
+                    if (words[j+i] != search[j]) {
+                        count --
+                        break
+                    }
+                }
+                count++
+            }
+        }
+
+        return count
     }
 }
