0ce616e0af94f7055241f6acb62e3ce70a22675c
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-15 07:15:22 +0700

    feat: (14-longest-common-prefix) optimize solution

--- easy/14-longest-common-prefix.kt
+++ easy/14-longest-common-prefix.kt
@@ -1,31 +1,28 @@
 class Solution {
     fun longestCommonPrefix(strs: Array<String>): String {
         var common = ""
-        val sortedStrs = strs.sortedBy { it.length }
+        if (strs.size == 1) return strs[0]
+        if (strs[0].length == 0) return common
 
-        if (sortedStrs.size == 1) return strs[0]
-        if (sortedStrs[0].length == 0) return common
-        
         var checking = true
-        var check = sortedStrs[0][0]
+        var check = strs[0][0]
         var index = 0
 
         while (checking) {
-            for (str in sortedStrs) {
-                if (str.length < index) {
+            for (str in strs) {
+                if (str.length -1 < index) {
                     checking = false
                     break
-                }
-                if (check != str[index]) {
+                }else if (check != str[index]) {
                     checking = false
                     break
                 }
             }
             if (checking) {
-                common += sortedStrs[0][index]
+                common += strs[0][index]
                 index ++
-                if (sortedStrs[0].length > index) {
-                    check = sortedStrs[0][index]
+                if (strs[0].length > index) {
+                    check = strs[0][index]
                 } else {
                     checking = false
                 }
