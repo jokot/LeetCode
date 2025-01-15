1dc9fc4da09d4f2e887458bab4da491e7b4da5ef
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-15 11:16:52 +0700

    feat: (14-longest-common-prefix) fully optimize solution
    - simpler checking and fastest

--- easy/14-longest-common-prefix.kt
+++ easy/14-longest-common-prefix.kt
@@ -1,31 +1,13 @@
 class Solution {
     fun longestCommonPrefix(strs: Array<String>): String {
-        var common = ""
-        if (strs.size == 1) return strs[0]
-        if (strs[0].length == 0) return common
+        var common = strs[0]
+        var length = strs[0].length
 
-        var checking = true
-        var check = strs[0][0]
-        var index = 0
-
-        while (checking) {
-            for (str in strs) {
-                if (str.length -1 < index) {
-                    checking = false
-                    break
-                }else if (check != str[index]) {
-                    checking = false
-                    break
-                }
-            }
-            if (checking) {
-                common += strs[0][index]
-                index ++
-                if (strs[0].length > index) {
-                    check = strs[0][index]
-                } else {
-                    checking = false
-                }
+        for (i in 1..<strs.size) {
+            while (common.length > strs[i].length || common != strs[i].substring(0, length)) {
+                length--
+                if (length == 0) return ""
+                common = common.substring(0, length)
             }
         }
 
