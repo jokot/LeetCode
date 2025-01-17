6987439c27bd669b7d6b440605e3d6c4c2c4373f
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-17 11:24:32 +0700

    feat(kt-217): optimize solution

--- kotlin/0217-e-contains-duplicate.kt
+++ kotlin/0217-e-contains-duplicate.kt
@@ -2,6 +2,16 @@
 class Solution {
     fun containsDuplicate(nums: IntArray): Boolean {
         if (nums.size < 2) return false
-        return nums.distinct().size != nums.size
+        val numsMap = hashMapOf<Int, Int>()
+
+        nums.forEach { num ->
+            numsMap[num] = numsMap.getOrDefault(num, 0) + 1
+        }
+
+        for (value in numsMap.values) {
+            if (value > 1) return true
+        }
+
+        return false
     }
 }
