675db0f391a1ee1d776cd2af08c6469e3ffd43ec
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-17 09:28:34 +0700

    feat(kt-1929): fully optimize solution

--- kotlin/1929-e-concatenation-of-array.kt
+++ kotlin/1929-e-concatenation-of-array.kt
@@ -1,5 +1,12 @@
+// https://leetcode.com/problems/concatenation-of-array
 class Solution {
     fun getConcatenation(nums: IntArray): IntArray {
-        return nums + nums
+        val n = nums.size
+        val ans = IntArray(n * 2)
+
+        System.arraycopy(nums, 0, ans, 0, n)
+        System.arraycopy(nums, 0, ans, n, n)
+
+        return ans
     }
 }
