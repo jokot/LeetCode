687276719f9a499730045f44816ab4c0c1ab03cf
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-17 09:04:19 +0700

    feat(py-1929): fully optimize solution

---
+++ python/1929-e-concatenation-of-array.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
+
+if __name__ == '__main__':
+    print(Solution().getConcatenation([1,2,3]))
