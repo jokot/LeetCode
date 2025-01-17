8cf5aeb2303c69300f352828f4cdfea6236b5530
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-17 09:04:46 +0700

    feat(py-1): add runner

--- python/0001-e-two-sums.py
+++ python/0001-e-two-sums.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
@@ -9,4 +10,7 @@ class Solution:
             seen[n] = i
 
         return []
+
+if __name__ == '__main__':
+    print(Solution().twoSum([1,2,3], 5))
         
