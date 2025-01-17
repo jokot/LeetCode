057106a96ae18df7bce33d038691c78649474ef0
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-17 10:51:07 +0700

    feat(py-217): initial solution

---
+++ python/0217-e-contains-duplicate.py
@@ -0,0 +1,10 @@
+## https://leetcode.com/problems/contains-duplicate
+from typing import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) < 2: return False
+        return len(set(nums)) != len(nums)
+
+if __name__ == '__main__':
+    print(Solution().containsDuplicate([1,2,3,4]))
+    print(Solution().containsDuplicate([1,2,3,4,1]))
