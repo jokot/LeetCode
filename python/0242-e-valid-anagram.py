ccdc09bdcd957c2802d782e3698803d5a0b41165
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-18 14:25:32 +0700

    feat(py-242): fully optimize solution

--- python/0242-e-valid-anagram.py
+++ python/0242-e-valid-anagram.py
@@ -1,17 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t): return False
-        sMap = {}
-        tMap = {}
+        charSet = set(s)
 
-        for i, c in enumerate(s):
-            sMap[c] = sMap.get(c, 0) + 1
-            tMap[t[i]] = tMap.get(t[i], 0) + 1
-
-        for key, val in sMap.items():
-            if tMap.get(key, 0) != val: return False
+        for c in charSet:
+            if (s.count(c) != t.count(c)):
+                return False
 
         return True
+        
 
 if __name__ == '__main__':
     print(Solution().isAnagram("anagram","gramana"))
