10212c19f00b53699d77076b5eda29f10a02c2e7
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-19 07:07:36 +0700

    feat(py-169): fully optimize solution.py

--- python/0169-e-majority-event.py
+++ python/0169-e-majority-event.py
@@ -2,18 +2,19 @@
 from typing import List
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums_map = {}
+        candidate = None
+        count = 0
 
-        for n in nums:
-            nums_map[n] = nums_map.get(n, 0) + 1
+        for num in nums:
+            if count == 0:
+                count = 1
+                candidate = num
+            elif num == candidate:
+                count += 1
+            else:
+                count -= 1
 
-        maxCount = max(nums_map.values())
-
-        for key, value in nums_map.items():
-            if maxCount == value:
-                return key
-
-        return 0
+        return candidate
 
 if __name__ == '__main__':
-    print(Solution().majorityElement([1,1,1,1,2,2,2,2,3,4,5,6]))
+    print(Solution().majorityElement([1,2,2,1,2,2,2,2,3,4,2,6]))
