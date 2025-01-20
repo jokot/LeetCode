8807cff848ee44f06d336fd85d1bd85b0d242f67
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-20 15:26:43 +0700

    feat(py-706): fully optimize solution

--- python/0706-e-design-hashmap.py
+++ python/0706-e-design-hashmap.py
@@ -1,26 +1,51 @@
 ## https://leetcode.com/problems/design-hashmap
+class ListNode:
+    def __init__(self, key, val, nxt):
+        self.key = key
+        self.val = val
+        self.next = nxt
+
 class MyHashMap:
 
     def __init__(self):
-        self.buckets = [None] * 1000001
-    
+        self.size = 1007
+        self.mult = 10000019
+        self.data = [None] * self.size
+
+    def hash(self, key) -> int:
+        return key * self.mult % self.size
+
     def put(self, key: int, value: int) -> None:
-        self.buckets[key] = value
+        self.remove(key)
+        index = self.hash(key)
+        node = ListNode(key, value, self.data[index])
+        self.data[index] = node
 
     def get(self, key: int) -> int:
-        val = self.buckets[key]
-        return val if val != None else -1
+        index = self.hash(key)
+        node = self.data[index]
+
+        while node:
+            if node.key == key:
+                return node.val
+            node = node.next
+
+        return -1
 
     def remove(self, key: int) -> None:
-        self.buckets[key] = None
+        index = self.hash(key)
+        node = self.data[index]
 
-    def get_values(self) -> list:
-        values = []
-        for i, n in enumerate(self.buckets):
-            if n != None:
-                values.append([i, n])
+        if not node: return
+        if node.key == key:
+            self.data[index] = node.next
+            return
 
-        return values
+        while node.next:
+            if node.next.key == key:
+                node.next = node.next.next
+                return
+            node = node.next
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
@@ -30,16 +55,15 @@ class MyHashMap:
 
 if __name__ == '__main__':
     my_map = MyHashMap()
-    print(my_map.get_values())
     my_map.put(1,1)
-    print(my_map.get_values())
     my_map.put(2,2)
-    print(my_map.get_values())
+    print(my_map.get(1))
+    print(my_map.get(2))
     my_map.remove(1)
-    print(my_map.get_values())
+    print(my_map.get(1))
     my_map.put(3,1)
-    print(my_map.get_values())
     my_map.put(4,2)
-    print(my_map.get_values())
+    print(my_map.get(3))
+    print(my_map.get(4))
     my_map.remove(3)
-    print(my_map.get_values())
+    print(my_map.get(3))
