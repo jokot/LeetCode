337169c71ba36d51a1f49c7b2463c4671f3a1b5d
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-21 17:01:53 +0700

    feat(py-125): simpler solution

--- python/0125-e-valid-palindrome.py
+++ python/0125-e-valid-palindrome.py
@@ -3,14 +3,8 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         cleanS = ""
         for c in s:
-            lower = ord(c) - ord('a')
-            upper = ord(c) - ord('A')
-            number = ord(c) - ord('0')
-
-            if lower in range(0, 26) or number in range(0, 10):
-                cleanS += c
-            elif upper in range(0, 26):
-                cleanS += chr(ord('a') + upper)
+            if c.isalnum():
+                cleanS += c.lower()
 
         for i in range(len(cleanS)//2):
             if cleanS[i] != cleanS[len(cleanS)-1-i]: return False
