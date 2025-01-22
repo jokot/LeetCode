5d75d061979fffdde54ed5433117db8c53627a16
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-22 22:04:54 +0700

    feat(kt-680): optimize alternative recursive solution

--- kotlin/0680-e-valid-palindrome-ii.kt
+++ kotlin/0680-e-valid-palindrome-ii.kt
@@ -1,17 +1,17 @@
 // https://leetcode.com/problems/valid-palindrome-ii
 class Solution {
     fun validPalindrome(s: String): Boolean {
-        return isPalindrome(s, 0, s.length - 1, false)
+        return isPalindrome(s, 0, s.length - 1, 1)
     }
 
-    fun isPalindrome(s: String, left: Int, right: Int, skip: Boolean): Boolean {
+    fun isPalindrome(s: String, left: Int, right: Int, bads: Int): Boolean {
+        if (bads < 0) return false
+
         return if (left < right) {
             if (s[left] == s[right]) {
-                isPalindrome(s, left+1, right-1, skip)
-            } else if (skip) {
-                false
+                isPalindrome(s, left+1, right-1, bads)
             } else {
-                isPalindrome(s, left+1, right, true) || isPalindrome(s, left, right-1, true)
+                isPalindrome(s, left+1, right, bads - 1) || isPalindrome(s, left, right-1, bads - 1)
             }
         } else true
     }
