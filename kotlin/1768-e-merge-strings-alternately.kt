de858bd14033398a7ff4a13d3f94dc486919dcc2
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-23 17:49:24 +0700

    feat(kt-1768): simpler and optimize solution

--- kotlin/1768-e-merge-strings-alternately.kt
+++ kotlin/1768-e-merge-strings-alternately.kt
@@ -1,21 +1,14 @@
 // https://leetcode.com/problems/merge-strings-alternately
 class Solution {
     fun mergeAlternately(word1: String, word2: String): String {
-        val len1 = word1.length
-        val len2 = word2.length
-        val max = if (len1 > len2) len1 else len2
-
-        var merged = ""
+        val merged = StringBuilder()
+        val max = maxOf(word1.length, word2.length)
 
         for (i in 0 until max) {
-            if (i < len1) {
-                merged += word1[i]
-            }
-            if (i < len2) {
-                merged += word2[i]
-            }
+            if (i < word1.length) merged.append(word1[i])
+            if (i < word2.length) merged.append(word2[i])
         }
 
-        return merged
+        return merged.toString()
     }
 }
