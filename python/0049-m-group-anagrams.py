c68654ff3332971e4777ca95ff991f7e64115452
Jokot <jokotriyanto12345@gmail.com>
2025-02-20 17:39:49 +0700

    feat(py-49): faster and simpler solution

--- python/0049-m-group-anagrams.py
+++ python/0049-m-group-anagrams.py
@@ -1,48 +1,26 @@
 # https://leetcode.com/problems/group-anagrams/
 
 from typing import List
+from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # create function is anagram
-        #     input two string
 
-        # create group anagram
-        # add first item to first group
-        # from index 1 to last index
-        #     do check is anagram with input form current string and first item form first group
-        #     if anagram then add it to the first group
-        #     else move to the next group
-        #         if next group empty
-        #             create new grop
-        #         else do the same checking (recursively / iteratively)
+        mapGroup = defaultdict(list)
 
-        group_anagrams = [[strs[0]]]
-
-        for i in range(1, len(strs)):
-            index, isAnagramOfGroups = self.isAnagramOfGroups(group_anagrams, strs[i])
-            if isAnagramOfGroups:
-                group_anagrams[index].append(strs[i])
-            else:
-                group_anagrams.append([strs[i]])
-        return group_anagrams
+        for s in strs:
+            key = self.createMapKey(s)
+            mapGroup[key].append(s)
+        
+        return list(mapGroup.values())
             
-    def isAnagramOfGroups(self, groups, str1):
-        for i, item in enumerate(groups):
-            if self.isAnagram(item[0], str1):
-                return (i, True)
-        return (-1, False)
+    def createMapKey(self, str1):
+        key = [0] * 26
 
-    def isAnagram(self, str1, str2):
-        if len(str1) != len(str2): return False
-
-        charS = set(str1)
-
-        for c in charS:
-            if str1.count(c) != str2.count(c):
-                return False
-                
-        return True
+        for c in str1:
+            key[ord(c) - ord('a')] += 1
+        
+        return tuple(key)
     
 # Runner and test cases
 def test_group_anagrams():
