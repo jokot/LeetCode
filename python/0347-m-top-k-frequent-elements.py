044f71922a58a9b685e6a5500bd0e0de0cf35fd4
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-02-24 06:00:47 +0700

    feat(py-374): add manual solution

--- python/0347-m-top-k-frequent-elements.py
+++ python/0347-m-top-k-frequent-elements.py
@@ -1,5 +1,5 @@
 # https://leetcode.com/problems/top-k-frequent-elements/
-from collections import Counter
+from collections import Counter, defaultdict
 from typing import List
 
 class Solution:
@@ -11,6 +11,43 @@ class Solution:
             top.append(n[0])
 
         return top
+
+class ManualSolution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if len(nums) == k:
+            return nums
+        
+        dic = self.counter(nums)
+        buckets = self.bucket_sort(len(nums), dic)
+        top = self.get_top(buckets, k)
+
+        return top
+    
+    def get_top(self, buckets, k):
+        top = []
+        for b in buckets[::-1]:
+            if len(b):
+                for e in b:
+                    top.append(e)
+                    if len(top) == k:
+                        return top
+        
+    
+    def counter(self, nums):
+        count = defaultdict(int)
+        for n in nums:
+            count[n] += 1
+        
+        return count
+
+    def bucket_sort(self, size, dic):
+        buckets = [[] for _ in range(size+1)]
+
+        for key, val in dic.items():
+            buckets[val].append(key)
+
+        return buckets
+
 
 # Runner code with sample input
 if __name__ == "__main__":
@@ -24,7 +61,8 @@ if __name__ == "__main__":
         ([4,1,-1,2,-1,2,3], 2, [-1,2])       # Negative numbers
     ]
     
-    solution = Solution()
+    solution = ManualSolution()
+    # solution = Solution()
     
     # Test each case
     for i, (nums, k, expected) in enumerate(test_cases, 1):
