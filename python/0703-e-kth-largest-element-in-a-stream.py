d7c6d6187a14f6c037aa431edd91fedac1936608
Jokot <jokotriyanto12345@gmail.com>
2025-03-05 22:16:40 +0700

    feat(py-703-wip): initial solution

---
+++ python/0703-e-kth-largest-element-in-a-stream.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.kth = k
+        self.nums = nums
+
+    def add(self, val: int) -> int:
+        self.nums.append(val)
+        return sorted(self.nums)[-self.kth]
+
+# Test runner
+if __name__ == "__main__":
+    # Test data
+    k = 3
+    nums = [4, 5, 8, 2]
+    kthLargest = KthLargest(k, nums)
+    print(kthLargest.add(3))  # returns 4
+    print(kthLargest.add(5))  # returns 5
+    print(kthLargest.add(10)) # returns 5
+    print(kthLargest.add(9))  # returns 8
+    print(kthLargest.add(4))  # returns 8
