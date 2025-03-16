fdceba7c9026f0ed6f2c75ffa4f10d0c92c437d9
jokot <jokotriyanto12345@gmail.com>
2025-03-16 21:14:34 +0700

    refactor(kt-205): optimize isomorphic strings solution using integer arrays for mapping

--- kotlin/0205-e-isomorphic-strings.kt
+++ kotlin/0205-e-isomorphic-strings.kt
@@ -1,14 +1,16 @@
 class Solution {
     fun isIsomorphic(s: String, t: String): Boolean {
-        val s_to_t = mutableMapOf<Char, Char>()
-        val t_to_s = mutableMapOf<Char, Char>()
+        val sToT = IntArray(256) { -1 }
+        val tToS = IntArray(256) { -1 }
 
-        for ((cs, ct) in s zip t) {
-            if ((cs in s_to_t && s_to_t[cs] != ct) || (ct in t_to_s && t_to_s[ct] != cs)) return false
-            else {
-                s_to_t[cs] = ct
-                t_to_s[ct] = cs
-            }
+        for (i in s.indices) {
+            val sCode = s[i].code
+            val tCode = t[i].code
+
+            if (sToT[sCode] != tToS[tCode]) return false
+
+            sToT[sCode] = i
+            tToS[tCode] = i
         }
 
         return true
