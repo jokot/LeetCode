48c7a779a3b35b97e13ce6aacbc8581f08085e48
jokot <jokotriyanto12345@gmail.com>
2025-04-06 23:46:35 +0700

    refactor(swift-1048): optimize string matching logic and simplify result collection

--- swift/1048-e-strign-matching-in-an-array.swift
+++ swift/1048-e-strign-matching-in-an-array.swift
@@ -1,22 +1,19 @@
 // https://leetcode.com/problems/string-matching-in-an-array/
 class Solution {
     func stringMatching(_ words: [String]) -> [String] {
-        if words.count == 1{
-            return []
-        }
 
         var result: [String] = []
 
-        for i in 0..<words.count-1 {
-            for k in i+1..<words.count {
-                if words[i].contains(words[k]) {
-                    result.append(words[k])
-                } else if words[k].contains(words[i]) {
-                    result.append(words[i])
+        for (i, val) in words.enumerated() {
+            let others = Array(words[..<i]) + Array(words[(i+1)..<words.count])
+            for o in others {
+                if o.contains(val) {
+                    result.append(val)
+                    break
                 }
             }
         }
 
-        return Array(Set(result))
+        return result
     }
 }
