24cb56f1092bccfa30a0514a46a4412fb243c24e
jokot <jokotriyanto12345@gmail.com>
2025-04-20 21:15:37 +0700

    feat(swift-2206): add optimize solution

--- swift/2206-e-devide-array-into-equal-pairs.swift
+++ swift/2206-e-devide-array-into-equal-pairs.swift
@@ -6,13 +6,12 @@ class Solution {
             seen[n] = seen[n, default: 0] + 1
         }
 
-        var totalPair = nums.count / 2
-
-        var count = 0
         for (key, val) in seen {
-            count += val / 2
+            if val%2 != 0 {
+                return false
+            }
         }
 
-        return count == totalPair
+        return true
     }
 }
