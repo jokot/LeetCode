621e7ac1115623e518e0c40b378bab8a1f5d9a74
jokot <jokotriyanto12345@gmail.com>
2025-05-10 22:12:22 +0700

    feat(swift-1913): add solutino with manual logic

--- swift/1913-e-maximum-product-difference-between-two-pairs.swift
+++ swift/1913-e-maximum-product-difference-between-two-pairs.swift
@@ -1,8 +1,27 @@
 // https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
 class Solution {
     func maxProductDifference(_ nums: [Int]) -> Int {
-        let sNums = nums.sorted()
-        let len = sNums.count
-        return sNums[len-2]*sNums[len-1] - sNums[0]*sNums[1]
+        var a = 10000
+        var b = 10000
+        var c = 0
+        var d = 0
+
+        for n in nums {
+            if n <= a {
+                b = a
+                a = n
+            } else if n <= b {
+                b = n
+            }
+
+            if n >= d {
+                c = d
+                d = n
+            } else if n >= c {
+                c = n
+            }
+        }
+
+        return c*d - a*b
     }
 }
